a894d5cee922926fe4af2727dd2fc812c431abbf
Rocio Romero <jazmi2019@hotmail.com>
2026-03-26 02:38:58 -0300

    agrego interfaces de especialidad

---
+++ Turnos.Domain/Interface/IEspecialidadRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turnos.Domain.Entities;
+
+namespace Turnos.Domain.Interface
+{
+    public interface IEspecialidadRepository
+    {
+        Task<IEnumerable<Especialidad>> ObtenerTodas();
+
+        Task<Especialidad?> ObtenerPorId(int id);
+
+        Task AgregarEspecialidad(Especialidad especialidad);
+
+        Task Actualizar(Especialidad especialidad);
+
+        Task Eliminar(int id);
+
+
+
+
+    }
+}
---
+++ Turnos.Domain/Interface/IEspecialidadService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turnos.Domain.Entities;
+
+
+
+
+namespace Turnos.Domain.Interface
+{
+    public interface IEspecialidadService
+    {
+        Task<IEnumerable<Especialidad>> ObtenerTodas();
+
+        Task<Especialidad?> ObtenerPorId(int id);
+
+        Task CrearEspecialidad(Especialidad especialidad);
+
+        Task ActualizarEspecialidad(int id, Especialidad especialidad);
+
+        Task EliminarEspecialidad(int id);
+
+       
+
+    }
+}
